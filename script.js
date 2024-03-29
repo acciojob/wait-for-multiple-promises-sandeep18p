4b7201c89783f1898d5f2f59ff7646f94df9526e
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-03-29 13:28:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-//your JS code here. If required.
+const ele = document.getElementById('output');
+
+function addRow(one = 'Loading...', two = 'Loading...') {
+    const row = document.createElement("tr");
+    const cell1 = document.createElement("td");
+    const cell2 = document.createElement("td");
+    cell1.textContent = one;
+    cell2.textContent = two;
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    ele.appendChild(row);
+}
+
+addRow();
+
+function createPromise() {
+    return new Promise((resolve) => {
+        const x = Math.random() * 3;
+        setTimeout(() => {
+            resolve(x);
+        }, 1000 * x);
+    });
+}
+
+const promises = [];
+
+for (let i = 1; i <= 3; i++) {
+    promises.push(createPromise());
+}
+
+Promise.all(promises)
+    .then((times) => {
+        ele.innerHTML = '';
+
+        let totalTime = 0;
+
+        times.forEach((time, index) => {
+            totalTime += time;
+            addRow(`Promise ${index + 1}`, `${time.toFixed(3)} seconds`);
+        });
+
+        addRow('Total', `${totalTime.toFixed(3)} seconds`);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
